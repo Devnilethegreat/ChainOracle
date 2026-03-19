7f0700e23a30648353a0a3df71308509f46f6f63
Devnilethegreat <212276580+Devnilethegreat@users.noreply.github.com>
2026-03-19 12:59:32 +0000

    Scaffold core module structure

---
+++ chainoracle.py
@@ -0,0 +1,14 @@
+# chainoracle.py
+"""
+Main module for ChainOracle application.
+"""
+
+import argparse
+import logging
+import os
+import sys
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
